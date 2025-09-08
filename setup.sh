3ad3f844d2bb0d99ea1825b2c095c3651abb27e0
sam <samlovelace12@gmail.com>
2025-09-07 20:00:35 -0400

    updates to use 4 less than nproc

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ LIB_INSTALL_DIR="/usr/local"
 WS="/home/robot_ws"
 
 # Subtract 2 from total cores
-CORES=$(( $(nproc) - 2 ))
+CORES=$(( $(nproc) - 4 ))
 
 # Ensure at least 1 core is used
 if [ "$CORES" -lt 1 ]; then
